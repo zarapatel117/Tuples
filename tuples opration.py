cae9eabc94c874108cffe6da391231dbfc13c598
zarapatel117 <zarapatel2510@gmail.com~>
2025-01-18 20:44:58 +0530

    m

---
+++ tuples opration.py
@@ -0,0 +1,14 @@
+tuples=("hi",2.3,4,True)
+print(tuples)
+
+tuples=(1,2,3,4,6,7,8)
+tuples=tuples+(9,)
+print(tuples)
+
+truples1=("hi")
+truples2=(" zara")
+print(truples1+truples2)
+
+truples=(1,3,5,6)
+_slice=truples[2:3]
+print(_slice)
